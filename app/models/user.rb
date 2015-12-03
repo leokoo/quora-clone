77b08d2c2b962afef99889d2c8fffc50f0ce5dbb
Leo Koo <leokoo@gmail.com>
2015-12-03 15:24:42 +0800

    making changes to the files
    BCrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
+	# users.password_hash in the database is a :string include BCrypt
 	validates :email, 
 		uniqueness: true,
 		format: { :with => /\w+(.)*\w+@\w+(.)*\w+/}
