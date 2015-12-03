6062fe6f3fea87f7a4fbba77a1fe799b45b67b09
Leo Koo <leokoo@gmail.com>
2015-12-04 02:22:46 +0800

    Session now works!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,3 +48,17 @@ get '/users/:id/edit' do
 end 
 
 # Delete
+
+helpers do 
+	# This will return the current user, if they exit
+	# Replace with code that works with your application
+	def current_user
+		if session[:user_id]
+			@current_user ||= User.find_by_id(session[:user_id])
+		end
+	end
+	# Return true if current_user exist, false others
+	def logged_in?
+		!current_user.nil?
+	end
+end
--- app/helpers/user.rb
+++ app/helpers/user.rb
@@ -1,20 +1,13 @@
 helpers do 
+enable :sessions
 	# This will return the current user, if they exit
-	# Replace with code that works with your application
 	def current_user
 		if session[:user_id]
-			@current_user ||= User.find_by_id(session)
+			@current_user ||= User.find_by_id(session[:user_id])
 		end
 	end
-
-	byebug
-
 	# Return true if current_user exist, false others
 	def logged_in?
 		!current_user.nil?
-	end
-
-	def users?
-		session[:user_id]
 	end
 end
