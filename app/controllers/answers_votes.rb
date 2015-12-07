c1833d7f7baf33508f8e187a3218e9010d17635b
Leo Koo <leokoo@gmail.com>
2015-12-07 22:14:50 +0800

    Answer upvotes/downvotes works!

---
+++ app/controllers/answers_votes.rb
@@ -0,0 +1,6 @@
+post "/answers/:id/upvote" do
+	answer = Answer.find(params[:id])
+	answer.votes.create(user_id: session[:user_id], vote_count: 1)
+	erb :'questions/all'
+	# question_vote.vote_count.to_json
+end
