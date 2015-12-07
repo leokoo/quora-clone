302e2881951de2d3cb1d2f9d9e697028c063e6fb
Leo Koo <leokoo@gmail.com>
2015-12-07 23:39:46 +0800

    Almost there but not yet! :(

--- app/controllers/answers_votes.rb
+++ app/controllers/answers_votes.rb
@@ -1,6 +1,21 @@
+# post "/answers/:id/upvote" do
+# 	answer = Answer.find(params[:id])
+# 	answer.answer_votes.create(user_id: session[:user_id], vote_count: 1)
+# 	erb :'questions/all'
+# 	# question_vote.vote_count.to_json
+# end
+
 post "/answers/:id/upvote" do
 	answer = Answer.find(params[:id])
 	answer.answer_votes.create(user_id: session[:user_id], vote_count: 1)
 	erb :'questions/all'
-	# question_vote.vote_count.to_json
+end
+
+# To update upvote
+post "/answers/:id/reset" do
+	answer = Answer.find(params[:id])
+	answer = answer.answer_votes.where(user_id: current_user.id)
+	byebug
+	answer.answer_votes.destroy
+	erb :'questions/all'
 end
--- app/models/answer_vote.rb
+++ app/models/answer_vote.rb
@@ -1,6 +1,6 @@
 class AnswerVote < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
-	validates_uniqueness_of :vote_count, scope: [:answer_id]
+	# validates_uniqueness_of :vote_count, scope: [:answer_id]
 	belongs_to :answer
 	belongs_to :user
 end
