99bc266fdabad156a8511286c8c78e818ba0ba43
Leo Koo <leokoo@gmail.com>
2015-12-08 06:25:19 +0800

    Now both question and answers vote hv been refactored!

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,48 +1,22 @@
 # To upvote
 post "/questions/:id/upvote" do
 	question = Question.find(params[:id])
-	question.votes.create(user_id: session[:user_id], vote_count: 1)
+	question.votes.create_with(vote_count: 1).find_or_create_by(user_id: session[:user_id]).update(vote_count: 1)
 	# question_vote = Vote.create(user_id: session[:user_id], question_id: params[:id], vote_count: 1)
-	# erb :'questions/all'
-	question_vote.vote_count.to_json
-end
-
-# To update upvote
-post "/upvote/:id/:vote/reset" do
-	votes = Vote.all
-	votes.each do |vote|
-		if vote.id == params[:vote].to_i
-		byebug
-			vote.update(vote_count: 1)
-		end
-	end
 	erb :'questions/all'
+	# question_vote.vote_count.to_json
 end
 
 # To downvote
-get "/questions/:id/downvote" do
-	question_vote = Vote.create(user_id: session[:user_id], question_id: params[:id], vote_count: -1)
+post "/questions/:id/downvote" do
+	question = Question.find(params[:id])
+	question.votes.create_with(vote_count: -1).find_or_create_by(user_id: session[:user_id]).update(vote_count: -1)
 	erb :'questions/all'
 end
 
 # To reset vote
-post "/questions/:id/:vote/reset" do
-	votes = Vote.all
-	votes.each do |vote|
-		if vote.id == params[:vote].to_i
-			vote.destroy
-		end
-	end
-	erb :'questions/all'
-end
-
-# To update downvote
-post "/downvote/:id/:vote/reset" do
-	votes = Vote.all
-	votes.each do |vote|
-		if vote.id == params[:vote].to_i
-			vote.update(vote_count: -1)
-		end
-	end
+post "/questions/:id/reset" do
+	question = Question.find(params[:id])
+	question.votes.where(user_id: current_user.id).destroy_all
 	erb :'questions/all'
 end
